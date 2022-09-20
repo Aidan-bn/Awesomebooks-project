978d6599c6431082c3f4ce573d53f8d2951e727c
Irirwanirira <irijoseph41@gmail.com>
2022-09-20 14:44:20 +0200

    Add Javascript function

---
+++ script.js
@@ -0,0 +1,37 @@
+const bookList = document.querySelector('.book-list');
+const form = document.querySelector('#book-form');
+
+// book collection
+let collection = [];
+
+/**
+ * Renders all the books in the array to the UI
+ * @param {string, string, number} title, author, bookId
+ */function addToBookList(title, author, bookId) {
+  // create all necessary variables
+  const bookTitle = document.createElement('p');
+  bookTitle.innerText = title;
+  const bookAuthor = document.createElement('p');
+  bookAuthor.innerText = author;
+  const removeBtn = document.createElement('button');
+  removeBtn.innerText = 'Remove';
+  const hr = document.createElement('hr');
+
+  // create container and append all these
+  const bookContainer = document.createElement('div');
+  bookContainer.appendChild(bookTitle);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(removeBtn);
+  bookContainer.appendChild(hr);
+  // append all this to the book list div element
+  bookList.appendChild(bookContainer);
+
+  // removing a book and updating the collection array
+  removeBtn.addEventListener('click', () => {
+    bookList.removeChild(bookContainer);
+    // update collection
+    collection = collection.filter((obj) => obj.bookId !== bookId);
+    // update local storage
+    localStorage.setItem('books', JSON.stringify(collection));
+  });
+}
